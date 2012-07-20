db9e4d17785aee6027f7001dccff742012582dd6
Michael Hartman <mphartman@me.com>
2012-07-20 14:05:05 -0500

    Initial commit

---
+++ TopPlaces/TopPlacesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TopPlacesAppDelegate.h
+//  TopPlaces
+//
+//  Created by Michael Hartman on 7/20/12.
+//  Copyright (c) 2012 Piwiggi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopPlaces/TopPlacesViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopPlacesViewController.h
+//  TopPlaces
+//
+//  Created by Michael Hartman on 7/20/12.
+//  Copyright (c) 2012 Piwiggi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopPlacesViewController : UIViewController
+
+@end
