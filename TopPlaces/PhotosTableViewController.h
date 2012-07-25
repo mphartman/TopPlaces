eba3c96543ba693ca256990c838091a80901f60b
Michael Hartman <mphartman@me.com>
2012-07-24 19:27:40 -0500

    Table view for photos for a selected place

---
+++ TopPlaces/PhotosTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosTableViewController.h
+//  TopPlaces
+//
+//  Created by Michael Hartman on 7/24/12.
+//  Copyright (c) 2012 Piwiggi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosTableViewController : UITableViewController
+@property (nonatomic, strong) NSArray *photosInPlace;
+@end
---
+++ TopPlaces/PhotosTableViewController.m
@@ -0,0 +1,119 @@
+//
+//  PhotosTableViewController.m
+//  TopPlaces
+//
+//  Created by Michael Hartman on 7/24/12.
+//  Copyright (c) 2012 Piwiggi. All rights reserved.
+//
+
+#import "PhotosTableViewController.h"
+#import "FlickrFetcher.h"
+
+@interface PhotosTableViewController ()
+
+@end
+
+@implementation PhotosTableViewController
+
+@synthesize photosInPlace = _photosInPlace;
+
+- (void)setPhotosInPlace:(NSArray *)photosInPlace 
+{
+    _photosInPlace = photosInPlace;
+    [self.tableView reloadData];
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.photosInPlace.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Photo Description";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    NSDictionary *photo = [self.photosInPlace objectAtIndex:indexPath.row];
+    cell.textLabel.text = [photo valueForKeyPath:FLICKR_PHOTO_TITLE];
+    cell.detailTextLabel.text = [photo valueForKeyPath:FLICKR_PHOTO_DESCRIPTION];
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+}
+
+@end
