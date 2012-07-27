8440648648f58e2a2a2fc03487d9095f859494ba
Michael Hartman <mphartman@me.com>
2012-07-27 12:14:15 -0500

    remove stub code

--- TopPlaces/TopPlacesTableViewController.m
+++ TopPlaces/TopPlacesTableViewController.m
@@ -36,32 +36,10 @@
     return nil;
 }
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
 - (void)viewWillAppear:(BOOL)animated
 {
     NSSortDescriptor *photoPlaceSortDescriptor = [[NSSortDescriptor alloc] initWithKey:FLICKR_PLACE_NAME ascending:YES];
-    NSLog(@"Loading top places from Flickr...");
+    //NSLog(@"Loading top places from Flickr...");
     self.topPlaces = [[FlickrFetcher topPlaces] sortedArrayUsingDescriptors:[NSArray arrayWithObject:photoPlaceSortDescriptor]];
 }
 
@@ -78,11 +56,11 @@
 {
     if ([segue.identifier isEqualToString:@"Show Photos of Place"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDictionary *topPlace = [self.topPlaces objectAtIndex:indexPath.row];
+        NSDictionary *place = [self.topPlaces objectAtIndex:indexPath.row];
         PhotosTableViewController *viewController = segue.destinationViewController;      
-        viewController.title = [self placeNameFromPhoto:topPlace];
-        NSLog(@"Loading photos for place from Flickr...");
-        viewController.photos = [FlickrFetcher photosInPlace:topPlace maxResults:MAX_PHOTOS];
+        viewController.title = [self placeNameFromPhoto:place];
+        //NSLog(@"Loading photos for place from Flickr...");
+        viewController.photos = [FlickrFetcher photosInPlace:place maxResults:MAX_PHOTOS];
     }
 }
 
@@ -111,52 +89,6 @@
     }
     
     return cell;
-}
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }   
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }   
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
-#pragma mark - UITableViewDelegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    
 }
 
 @end
